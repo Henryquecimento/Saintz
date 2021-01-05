d67e83865163de2aa000fe95b37965a8c05cc455
Henryque <henryque_1@hotmail.com>
2021-01-05 17:35:28 -0300

    adding productData.js (05/01/21)

---
+++ productData.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        prodName: 'Travel Bagpack',
+        price: '185.00'
+    },
+    {
+        prodName: 'HandBag',
+        price: '45.00'
+    },
+    {
+        prodName: 'Bagpack',
+        price: '120.00'
+    },
+    {
+        prodName: 'RollBag',
+        price: '35.00'
+    },
+    {
+        prodName: 'Business Bag',
+        price: '95.00'
+    },
+    {
+        prodName: 'Black Bagpack',
+        price: '120.00'
+    },
+    {
+        prodName: 'ArtBag',
+        price: '35.00'
+    },
+    {
+        prodName: 'Model Bag',
+        price: '95.00'
+    }
+]
