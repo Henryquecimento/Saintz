5ed0b658ef54abea92029a2df39c3f5e2aaf4687
henryque <henryque_1@hotmail.com>
2021-09-05 19:25:05 -0300

    feat(scripts): add GalleryImage object and setImage method to handle image changing.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -110,3 +110,17 @@ const PhotosUpload = {
   }
 
 }
+
+const ImageGallery = {
+  highlight: document.querySelector('.gallery div.highlight > img'),
+  previews: document.querySelectorAll('.gallery div.gallery-preview img'),
+  setImage(event) {
+    const { target } = event;
+
+    ImageGallery.previews.forEach(preview => preview.classList.remove('active'));
+
+    target.classList.add('active');
+
+    ImageGallery.highlight.src = target.src;
+      }
+}
