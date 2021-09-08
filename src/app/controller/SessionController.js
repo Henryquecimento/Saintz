d36bd69e57c35a62c78f723dc851250177f23f58
henryque <henryque_1@hotmail.com>
2021-09-08 18:13:56 -0300

    feat: add SessionController and its methods

---
+++ src/app/controller/SessionController.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+	loginForm(req, res) {
+
+		return res.render('admin/users/login.njk')
+	}
+	/* ,
+	login(req, res) {
+
+	},
+	logout(req, res) {
+
+	},
+	resetForm(req, res) {
+
+	},
+	reset(req, res) {
+
+	} 
+	*/
+
+};
