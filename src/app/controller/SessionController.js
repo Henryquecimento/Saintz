dd22bcfa82ae53db155111c5d3c8dd6c80fb1d5a
henryque <henryque_1@hotmail.com>
2021-09-09 19:41:38 -0300

    feat(SessionController): add login method

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -3,11 +3,14 @@ module.exports = {
 	loginForm(req, res) {
 
 		return res.render('admin/users/login.njk')
-	}
-	/* ,
-	login(req, res) {
-
 	},
+	login(req, res) {
+		req.session.userId = req.user.id;
+		req.session.isAdmin = req.user.is_admin;
+
+		return res.redirect('/admin/products');
+	},
+	/* ,
 	logout(req, res) {
 
 	},
