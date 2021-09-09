c1f420facab69a3783aed42379e0a89135d89671
henryque <henryque_1@hotmail.com>
2021-09-09 19:42:53 -0300

    feat: add login routes and its validators middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const SessionController = require('../app/controller/SessionController');
 
+const sessionValidators = require('../app/validators/session');
+
 const routes = express.Router();
 
 routes.get('/login', SessionController.loginForm);
+routes.post('/login', sessionValidators.login, SessionController.login);
 
 module.exports = routes;
