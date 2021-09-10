90e84eafb2969a71376e301787e98a052981fe36
henryque <henryque_1@hotmail.com>
2021-09-10 19:01:28 -0300

    refactor(validators/session): readjust users validation

--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -1,11 +1,17 @@
 const User = require('../models/users');
 
 async function login(req, res, next) {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const user = await User.findOne({
     where: { email }
   });
+
+  if (!user) return res.send('User does not exist!');
+
+  /* const passed = '1234'; */
+
+  /* if (!password != '1234') return res.send('Invalid email or password'); */
 
   req.user = user;
 
