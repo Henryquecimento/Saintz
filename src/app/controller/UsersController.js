ae412296fafb81c0a2b0d56101fce07d585cfb8d
henryque <henryque_1@hotmail.com>
2021-09-12 20:20:57 -0300

    feat(UsersController): add nodemailer to handle messages sent to users

--- src/app/controller/UsersController.js
+++ src/app/controller/UsersController.js
@@ -1,7 +1,7 @@
 const User = require('../models/users');
 const crypto = require('crypto');
 const { hash } = require('bcrypt');
-
+const mailer = require('../../lib/mailer');
 
 module.exports = {
 	index(req, res) {
@@ -15,6 +15,30 @@ module.exports = {
 
 		const firstPassword = crypto.randomBytes(20).toString('hex');
 		const encryptedPassword = await hash(firstPassword, 8);
+
+		await mailer.sendMail({
+			to: email,
+			from: 'no-reply@saintz.com',
+			subject: "User's Subscription",
+			html: `<h2>Hello ${name}, welcome to Saintz!</h2>
+			<p>Your login and temporary password is down below:</p>
+			<p>
+				Email: ${email}</br>
+				Password: ${encryptedPassword}
+			</p>
+			<p>
+				<a href="http://localhost:4000/users/login" target='_blank'>
+				ACCESS WITH YOUR LOGIN
+				</a>		
+			</p>
+			<p>If you want to change your password now, please access the link down below:</p>
+			<p>
+				<a href="http://localhost:4000/users/forgot-password" target='_blank'>
+				CHANGE YOUR PASSWORD
+				</a>		
+			</p>
+			`
+		});
 
 		await User.post({
 			name,
