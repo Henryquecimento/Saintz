231a5725934e9f52efee1d1baca9635bf50908a7
henryque <henryque_1@hotmail.com>
2021-09-14 19:02:34 -0300

    feat(SessionController): add resetForm and reset methods to handle password reset

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -1,6 +1,7 @@
 const User = require('../models/users');
 const mailer = require('../../lib/mailer');
 const crypto = require('crypto');
+const { hash } = require('bcrypt');
 
 module.exports = {
 	loginForm(req, res) {
@@ -49,14 +50,38 @@ module.exports = {
 
 
 		return res.render('admin/session/login.njk', { token });
-	}
-	/* ,
-	resetForm(req, res) {
-
 	},
-	reset(req, res) {
+	resetForm(req, res) {
+		return res.render('admin/session/reset-password.njk');
+	},
+	async reset(req, res) {
+		const { user } = req;
+		const { password } = req.body;
 
-	} 
-	*/
+		const passwordEncrytion = await hash(password, 8);
 
+
+		await User.update(user.id, {
+			password: passwordEncrytion,
+			reset_token: '',
+			reset_token_expires: ''
+		});
+
+		await mailer.sendMail({
+			to: user.email,
+			from: 'no-reply@saintz.com',
+			subject: "New Password",
+			html: `<h2>Hello ${user.name}! We have great news for you!</h2>
+			<p>You password was recovered successfully!</p>
+			<p>Now you can access your account and enjoy the SAINTZ plataform in the link below:</p>
+			<p>
+				<a href="http://localhost:4000/users/login" target='_blank'>
+				ACCESS YOUR ACCOUNT
+				</a>		
+			</p>
+			`
+		});
+
+		return res.render('admin/session/login.njk')
+	}
 };
