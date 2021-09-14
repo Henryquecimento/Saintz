ef38b2cfee408e6aa75a59f3c9675a6b87a93c26
henryque <henryque_1@hotmail.com>
2021-09-14 17:59:44 -0300

    feat(SessionController): add forgotForm and forgot methods to handle user forgot-password page

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -1,3 +1,6 @@
+const User = require('../models/users');
+const mailer = require('../../lib/mailer');
+const crypto = require('crypto');
 
 module.exports = {
 	loginForm(req, res) {
@@ -15,6 +18,38 @@ module.exports = {
 
 		return res.redirect('/');
 	},
+	forgotForm(req, res) {
+		return res.render('admin/session/forgot-password.njk');
+	},
+	async forgot(req, res) {
+		const { user } = req;
+
+		const token = crypto.randomBytes(20).toString('hex');
+		let now = new Date();
+		now = now.setHours(now.getHours() + 1);
+
+		await User.update(user.id, {
+			reset_token: token,
+			reset_token_expires: now
+		})
+
+		await mailer.sendMail({
+			to: user.email,
+			from: 'no-reply@saintz.com',
+			subject: "Password Recovery",
+			html: `<h2>Hello ${user.name}! Forgot your password?</h2>
+			<p>No problem, you can recovery your password clicking in the link below:</p>
+			<p>
+				<a href="http://localhost:4000/users/reset-password?${token}" target='_blank'>
+				RECOVER YOUR PASSWORD
+				</a>		
+			</p>
+			`
+		});
+
+
+		return res.render('admin/session/login.njk', { token });
+	}
 	/* ,
 	resetForm(req, res) {
 
