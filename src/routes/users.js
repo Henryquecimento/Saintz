5238520db512f360b119a29cb541c4596e67f3f0
henryque <henryque_1@hotmail.com>
2021-09-15 18:09:33 -0300

    feat: add profile page and its index and update routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,14 +2,15 @@ const express = require("express");
 
 const SessionController = require('../app/controller/SessionController');
 const UsersController = require('../app/controller/UsersController');
+const ProfileController = require('../app/controller/ProfileController');
 
 const sessionValidators = require('../app/validators/session');
+const userValidators = require('../app/validators/users');
 
 const routes = express.Router();
 
 routes.get('/login', SessionController.loginForm);
 routes.post('/login', sessionValidators.login, SessionController.login);
-
 routes.post('/logout', SessionController.logout);
 routes.get('/forgot-password', SessionController.forgotForm);
 routes.post('/forgot-password', sessionValidators.forgot, SessionController.forgot);
@@ -22,5 +23,8 @@ routes.get('/', UsersController.index);
 
 routes.get('/create', UsersController.create);
 routes.post('/', UsersController.post);
+
+routes.get('/profile', userValidators.show, ProfileController.index);
+routes.put('/profile', userValidators.update, ProfileController.put);
 
 module.exports = routes;
