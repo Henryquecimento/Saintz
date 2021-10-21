b18ea6006794c61a9aab40fd0c18fa7753fdba3a
henryque <henryque_1@hotmail.com>
2021-10-20 21:26:27 -0300

    feat(routes/private): add onlyUsers middleware to private routes

--- src/routes/private.js
+++ src/routes/private.js
@@ -4,21 +4,23 @@ const multer = require("../app/middlewares/multer");
 const Categories = require("../app/controller/CategoriesController");
 const Products = require("../app/controller/ProductsController");
 
+const { onlyUsers } = require('../app/middlewares/session');
+
 const routes = express.Router();
 
-routes.get('/categories', Categories.index);
-routes.get('/categories/create', Categories.create);
-routes.get('/categories/:id', Categories.show);
-routes.get('/categories/:id/edit', Categories.edit);
+routes.get('/categories', onlyUsers, Categories.index);
+routes.get('/categories/create', onlyUsers, Categories.create);
+routes.get('/categories/:id', onlyUsers, Categories.show);
+routes.get('/categories/:id/edit', onlyUsers, Categories.edit);
 
 routes.post("/categories", Categories.post);
 routes.put("/categories", Categories.put);
 routes.delete("/categories", Categories.delete);
 
-routes.get("/products", Products.index);
-routes.get("/products/create", Products.create);
-routes.get("/products/:id", Products.show);
-routes.get("/products/:id/edit", Products.edit);
+routes.get("/products", onlyUsers, Products.index);
+routes.get("/products/create", onlyUsers, Products.create);
+routes.get("/products/:id", onlyUsers, Products.show);
+routes.get("/products/:id/edit", onlyUsers, Products.edit);
 
 routes.post("/products", multer.array("photo", 4), Products.post);
 routes.put("/products", multer.array("photo", 4), Products.put);
