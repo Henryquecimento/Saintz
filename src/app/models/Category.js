0f387a7db47e9c545882167c082f7ffe567412cd
henryque <henryque_1@hotmail.com>
2021-11-01 12:35:34 -0300

    refactor(Category): remove unnecessary methods

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -5,20 +5,6 @@ Base.init({ table: 'categories' });
 
 module.exports = {
   ...Base,
-  all() {
-    return db.query(`
-      SELECT * 
-      FROM categories
-      ORDER BY id
-    `);
-  },
-  find(id) {
-    return db.query(`
-      SELECT categories.*
-      FROM categories
-      WHERE categories.id = $1
-    `, [id]);
-  },
   update(data) {
     const query = `
       UPDATE categories SET
