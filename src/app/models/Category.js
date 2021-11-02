5d6c7e03ad75dda4ad7796daf3c9e5d7caa982ea
henryque <henryque_1@hotmail.com>
2021-11-02 17:46:13 -0300

    refactor(Category): remove unnecessary methods

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -1,14 +1,7 @@
-const db = require("./../../config/db");
 const Base = require('./Base');
 
 Base.init({ table: 'categories' });
 
 module.exports = {
-  ...Base,
-  delete(id) {
-    return db.query(`
-      DELETE FROM categories  
-      WHERE id = $1
-    `, [id])
-  }
+  ...Base
 };
