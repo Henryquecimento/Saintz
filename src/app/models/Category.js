582faf2e08b0b98225772a5ed64c7d380e407501
henryque <henryque_1@hotmail.com>
2021-11-02 17:13:54 -0300

    refactor(Category): remove unecessary methods

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -5,20 +5,6 @@ Base.init({ table: 'categories' });
 
 module.exports = {
   ...Base,
-  update(data) {
-    const query = `
-      UPDATE categories SET
-        name = ($1)
-      WHERE id = $2    
-    `;
-
-    const values = [
-      data.name,
-      data.id
-    ];
-
-    return db.query(query, values);
-  },
   delete(id) {
     return db.query(`
       DELETE FROM categories  
