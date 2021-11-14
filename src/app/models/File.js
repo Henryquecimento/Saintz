6939222ac7659d18b0080df634cf7162800a4189
henryque <henryque_1@hotmail.com>
2021-11-14 15:31:25 -0300

    refactor(File): readjust to handle commmon Base methods

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,14 +1,9 @@
 const db = require('../../config/db');
 
-module.exports = {
-  async create({ name, path }) {
+const Base = require("./Base");
 
-    return db.query(`
-      INSERT INTO files (
-        name,
-        path
-      ) VALUES ($1, $2)
-      RETURNING id    
-    `, [name, path]);
-  }
+Base.init({ table: "files" });
+
+module.exports = {
+  ...Base,
 }
