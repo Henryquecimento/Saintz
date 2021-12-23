2d56f3c0c3a3aea5c06e137e302772e83645d9e4
henryque <henryque_1@hotmail.com>
2021-12-23 17:50:26 -0300

    refactor(UsersController): readjust send email functionality

--- src/app/controller/UsersController.js
+++ src/app/controller/UsersController.js
@@ -4,6 +4,32 @@ const { hash } = require('bcrypt');
 const mailer = require('../../lib/mailer');
 const { LoadUsers } = require('../services/LoadUserServices');
 
+function SendEmail(name, email, password) {
+	return {
+		to: email,
+		from: 'no-reply@saintz.com',
+		subject: "User's Subscription",
+		html: `<h2>Hello ${name}, welcome to Saintz!</h2>
+		<p>Your login and temporary password is down below:</p>
+		<p>
+			Email: ${email}</br>
+			Password: ${password}
+		</p>
+		<p>
+			<a href="http://localhost:4000/users/login" target='_blank'>
+			ACCESS WITH YOUR LOGIN
+			</a>		
+		</p>
+		<p>If you want to change your password now, please access the link down below:</p>
+		<p>
+			<a href="http://localhost:4000/users/forgot-password" target='_blank'>
+			CHANGE YOUR PASSWORD
+			</a>		
+		</p>
+		`
+	}
+}
+
 module.exports = {
 	async index(req, res) {
 		const users = await LoadUsers.load("users");
@@ -19,29 +45,7 @@ module.exports = {
 		const firstPassword = crypto.randomBytes(20).toString('hex');
 		const encryptedPassword = await hash(firstPassword, 8);
 
-		await mailer.sendMail({
-			to: email,
-			from: 'no-reply@saintz.com',
-			subject: "User's Subscription",
-			html: `<h2>Hello ${name}, welcome to Saintz!</h2>
-			<p>Your login and temporary password is down below:</p>
-			<p>
-				Email: ${email}</br>
-				Password: ${encryptedPassword}
-			</p>
-			<p>
-				<a href="http://localhost:4000/users/login" target='_blank'>
-				ACCESS WITH YOUR LOGIN
-				</a>		
-			</p>
-			<p>If you want to change your password now, please access the link down below:</p>
-			<p>
-				<a href="http://localhost:4000/users/forgot-password" target='_blank'>
-				CHANGE YOUR PASSWORD
-				</a>		
-			</p>
-			`
-		});
+		await mailer.sendMail(SendEmail(name, email, encryptedPassword));
 
 		await User.create({
 			name,
